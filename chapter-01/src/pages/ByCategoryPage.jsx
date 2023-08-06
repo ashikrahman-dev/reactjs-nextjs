4e9ef0137dbcab3498a444afb10f62006fd379f6
Ashik <ashikur1993@gmail.com>
2023-08-07 00:04:58 +0600

    Add Category page

--- chapter-01/src/pages/ByCategoryPage.jsx
+++ chapter-01/src/pages/ByCategoryPage.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
+import { postList } from "../ApiRequest/ApiRequest";
+import Layout from "../layout/layout";
+import Loader from "../component/Loader";
+import BlogList from "../component/BlogLIst";
+import { useParams } from "react-router-dom";
 
 
 const ByCategoryPage = () => {
+    let {id} = useParams();
+    const [list, setList] = useState(null);
+
+    useEffect( () => {
+        (async () => {
+            let res = await postList(id);
+            setList(res);
+        })()
+    }, [id] )
+
     return (
-        <div>
-            By Category Page
-        </div>
+        <Layout>
+            {
+                list === null ? <Loader /> : <BlogList list={list} />
+            }
+        </Layout>
     );
 };
 
