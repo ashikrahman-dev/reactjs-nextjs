a4e1c9949255d949704e0117e70dfb7a34ac7e02
ashikrahman-dev <ashikur1993@gmail.com>
2023-09-07 11:00:06 +0600

    Login page and cart responsive

--- chapter-02/src/pages/HomePage.jsx
+++ chapter-02/src/pages/HomePage.jsx
@@ -5,8 +5,8 @@ export default function Homepage() {
     return (
         <>
             <Layout>
-                <section className="container mx-auto min-h-screen py-12">
-                    <div className="grid grid-cols-4 gap-4">
+                <section className="container mx-auto min-h-screen py-12 px-3">
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                         <Card />
                         <Card />
                         <Card />
