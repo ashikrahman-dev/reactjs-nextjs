1b79c35bd2715354ac4aa626df2932e06eaa2766
ashikrahman-dev <ashikur1993@gmail.com>
2023-10-05 15:57:56 +0600

    Dark mode and light mode using react js

---
+++ chapter-04/dark-mode/src/components/ThemeSwitcher.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+import DarkIcon from "./DarkIcon";
+import LightIcon from "./LightIcon";
+
+export default function ThemeSwitcher() {
+    const [theme, setTheme] = useState(null);
+
+    useEffect(() => {
+        if (window.matchMedia("prefer-color-schema: dark").matches) {
+            setTheme("dark");
+        } else {
+            setTheme("light");
+        }
+    }, []);
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [theme]);
+
+    const handleThemeSwitch = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
+    return (
+        <>
+            <div className=" dark:text-white">Lorem ipsum.</div>
+            <button
+                onClick={handleThemeSwitch}
+                className="bg-sky-500 hover:bg-sky-600 hover:text-white font-medium transition-all delay-50 rounded-full px-7 py-2 flex gap-2 buttonWrap"
+            >
+                Dark
+                {theme === "dark" ? <LightIcon /> : <DarkIcon />}
+            </button>
+        </>
+    );
+}
